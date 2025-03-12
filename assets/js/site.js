99f78e74b095652f0b5735682dbd84e4e56b895d
Fillip-M <fillipmosgaard@gmail.com>
2025-03-12 10:44:40 +0100

    Finished the project

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,13 +1,24 @@
 // write cool JS here!!
-const myInfo = 'en variabel';
-const myBooks = ["The Catcher in the Rye",
-    "To Kill a Mockingbird",
-    "1984",
-    "Pride and Prejudice",
-    "The Great Gatsby",
-    "The Hobbit",
-    "Harry Potter and the Sorcerer's Stone",
-    "The Lord of the Rings",
-    "The Da Vinci Code",
-    "The Hunger Games"]
+const myInfo = 'a variable';
+const myBooks = [
+  'The Catcher in the Rye',
+  'To Kill a Mockingbird',
+  '1984',
+  'Pride and Prejudice',
+  'The Great Gatsby',
+  'The Hobbit',
+  "Harry Potter and the Sorcerer's Stone",
+  'The Lord of the Rings',
+  'The Da Vinci Code',
+  'The Hunger Games',
+];
+console.groupCollapsed('console project');
+console.log('i am a normal console log');
+console.info('i am a console info: ' + myInfo);
+console.warn('this is a warning');
+console.error('this is an error');
 
+console.table(myBooks);
+console.time('default');
+console.timeEnd('default');
+console.groupEnd();
